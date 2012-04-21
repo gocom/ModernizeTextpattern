9dfdce5db732ab824f28cc010382f09fde7a9e7a
Jukka Svahn <web@rahinaa.biz>
2012-04-21 13:19:17 +0300

    Multi-selector, multi-match support, chaining.

--- js/search.js
+++ js/search.js
@@ -8,35 +8,40 @@
 
 (function($){
 	$.fn.rah_search = function() {
+		
+		return this.each(function() {
+			
+			var $this = $(this);
 	
-		var form = {
-			label : this.find('label'),
-			button : this.find('[type=submit]'),
-			field : this.find('input[type=text]'),
-			options : this.find('select')
-		};
-		
-		if(!form.field.val()) {
-			form.options.hide();
-		}
-		
-		form.button.remove();
-		form.field.attr('placeholder', form.label.text()+'…');
-		form.label.remove();
-		
-		form.field.bind('keyup input change blur', function() {
-			if($(this).val()) {
-				form.options.show();
-			}
-			else {
+			var form = {
+				label : $this.find('label'),
+				button : $this.find('[type=submit]'),
+				field : $this.find('input[type=text]'),
+				options : $this.find('select')
+			};
+			
+			if(!form.field.val()) {
 				form.options.hide();
 			}
-		});
-		
-		form.options.bind('change blur', function() {
-			if(form.field.val()) {
-				$(this).parents('form').submit();
-			}
+			
+			form.button.remove();
+			form.field.attr('placeholder', form.label.text()+'…');
+			form.label.remove();
+			
+			form.field.bind('keyup input change blur', function() {
+				if($(this).val()) {
+					form.options.show();
+				}
+				else {
+					form.options.hide();
+				}
+			});
+			
+			form.options.bind('change blur', function() {
+				if(form.field.val()) {
+					$(this).parents('form').submit();
+				}
+			});
 		});
 	};
 })(jQuery);
