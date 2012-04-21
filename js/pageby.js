51d63ef73fa306860a2d8f44d5ec892448e4ee7f
Jukka Svahn <web@rahinaa.biz>
2012-04-21 13:14:48 +0300

    Fixed multi-selectors, chaining.

--- js/pageby.js
+++ js/pageby.js
@@ -9,22 +9,27 @@
 (function($){
 	$.fn.rah_pageby = function() {
 		
-		var form = {
-			q : this.find('input:hidden').serialize(),
-			to : this.find('select[name="qty"]').parent(),
-			selected : this.find('select[name="qty"] option:selected').val()
-		};
-		
-		$(this).find('select[name="qty"] option').each(function(i) {
-			var qty = $(this).val();
+		return this.each(function() {
 			
-			if(i) {
-				form.to.append('<span class="divider"> | </span>');
-			}
+			var $this = $(this);
 		
-			form.to.append(' <a class="pagebylink'+(form.selected == qty ? ' active' : '')+'" href="?'+form.q+'&qty='+qty+'">'+qty+'</a> ');		
+			var form = {
+				q : $this.find('input:hidden').serialize(),
+				to : $this.find('select[name="qty"]').parent(),
+				selected : $this.find('select[name="qty"] option:selected').val()
+			};
+			
+			$this.find('select[name="qty"] option').each(function(i) {
+				var qty = $(this).val();
+				
+				if(i) {
+					form.to.append('<span class="divider"> | </span>');
+				}
+			
+				form.to.append(' <a class="pagebylink'+(form.selected == qty ? ' active' : '')+'" href="?'+form.q+'&qty='+qty+'">'+qty+'</a> ');		
+			});
+			
+			$this.find('select[name="qty"]').remove();
 		});
-		
-		this.find('select[name="qty"]').remove();
 	};
 })(jQuery);
