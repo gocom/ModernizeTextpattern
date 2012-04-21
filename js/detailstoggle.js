2203b479c8bfd267e960ec42136f0aa0618127b1
Jukka Svahn <web@rahinaa.biz>
2012-04-21 13:12:55 +0300

    Chaining support, performance optimization.

--- js/detailstoggle.js
+++ js/detailstoggle.js
@@ -8,13 +8,17 @@
 
 (function($){
 	$.fn.rah_detailstoggle = function() {
-		var form = {
-			toggle : this.find('input[type=checkbox]'),
-			label : this.find('label')
-		};
-
-		this.parents('table').find('thead tr th:last-child').html(form.toggle);
-		form.toggle.attr('title', form.label.text());
-		form.label.remove();
+		return this.each(function() {
+			var $this = $(this);
+		
+			var form = {
+				toggle : $this.find('input[type=checkbox]'),
+				label : $this.find('label')
+			};
+	
+			$this.parents('table').find('thead tr th:last-child').html(form.toggle);
+			form.toggle.attr('title', form.label.text());
+			form.label.remove();
+		});
 	};
 })(jQuery);
