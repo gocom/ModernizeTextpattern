7ec339f0425d01943b71a7b72a58e9703398f85d
Jukka Svahn <web@rahinaa.biz>
2012-04-21 13:17:11 +0300

    Fixed multi-selectors, chaining.

--- js/pagination.js
+++ js/pagination.js
@@ -9,33 +9,38 @@
 (function($){
 	$.fn.rah_pagination = function() {
 		
-		var form = {
-			q : this.find('input:hidden').serialize(),
-			selected : parseInt(this.find('select[name="page"] option:selected').val()),
-			pages : this.find('select[name="page"]')
-		};
-		
-		if(form.pages.length < 1) {
-			this.remove();
-			return;
-		}
-		
-		form.prev = form.pages.prev('.navlink');
-		form.next = form.pages.next('.navlink');
-		
-		form.min = form.selected-5;
-		form.max = form.selected+5;
-		
-		form.pages.find('option').each(function(i) {
-			var page = parseInt($(this).val());
+		return this.each(function() {
 			
-			if(page > form.max || page < form.min) {
+			var $this = $(this);
+		
+			var form = {
+				q : $this.find('input:hidden').serialize(),
+				selected : parseInt($this.find('select[name="page"] option:selected').val()),
+				pages : $this.find('select[name="page"]')
+			};
+			
+			if(form.pages.length < 1) {
+				$this.remove();
 				return;
 			}
-		
-			form.pages.before(' <a class="navlink'+(form.selected == page ? ' active' : '')+'" href="?'+form.q+'&page='+page+'">'+page+'</a> ');
+			
+			form.prev = form.pages.prev('.navlink');
+			form.next = form.pages.next('.navlink');
+			
+			form.min = form.selected-5;
+			form.max = form.selected+5;
+			
+			form.pages.find('option').each(function(i) {
+				var page = parseInt($(this).val());
+				
+				if(page > form.max || page < form.min) {
+					return;
+				}
+			
+				form.pages.before(' <a class="navlink'+(form.selected == page ? ' active' : '')+'" href="?'+form.q+'&page='+page+'">'+page+'</a> ');
+			});
+			
+			form.pages.remove();
 		});
-		
-		form.pages.remove();
 	};
 })(jQuery);
