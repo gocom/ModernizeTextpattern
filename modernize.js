be60075c4fa8f4fdbb34aac71a4a3419e078307b
Jukka Svahn <web@rahinaa.biz>
2012-06-28 05:56:30 +0300

    Multi-edit and modifier key integration is now part of the dev branch (core).

--- modernize.js
+++ modernize.js
@@ -3,7 +3,6 @@
  */
 
 $(document).ready(function(){
-	$('#page-list .txp-list, #page-image .txp-list, #page-file .txp-list, #page-link .txp-list, #page-discuss .txp-list, #page-admin .txp-list, #page-log .txp-list, #page-plugin .txp-list, #page-section .txp-list').rah_multiedit();
 	$('.search-form').rah_search();
 	$('.pageby').rah_pageby();
 	$('.detail-toggle').rah_detailstoggle();
