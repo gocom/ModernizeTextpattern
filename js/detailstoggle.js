b18b55cfcb635075cb145e4a4097a1ff97505de8
Jukka Svahn <web@rahinaa.biz>
2012-09-07 20:59:40 +0300

    Make details column toggle visible, but show it after page by links in the footer.

--- js/detailstoggle.js
+++ js/detailstoggle.js
@@ -8,18 +8,7 @@
 
 (function($){
 	$.fn.rah_detailstoggle = function() {
-		return this.each(function() {
-			var $this = $(this);
-			
-			var form = {
-				thead : $('.txp-list thead th'),
-				label : $this.find('label').text()
-			};
-			
-			form.thead.dblclick(function(e) {
-				e.preventDefault();
-				$this.find('input[type=checkbox]').click();
-			});
-		}).hide();
+		$('.detail-toggle').appendTo('.pageby');
+		return;
 	};
 })(jQuery);
