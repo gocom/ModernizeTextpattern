c4632fe5224344623c912a0cb558f3a92f7cdff7
Jukka Svahn <web@rahinaa.biz>
2012-09-10 05:24:51 +0300

    Move show spam toggle to the footer too.

--- modernize.js
+++ modernize.js
@@ -5,6 +5,6 @@
 $(document).ready(function(){
 	$('.search-form').rah_search();
 	$('.pageby').rah_pageby();
-	$('.detail-toggle').rah_detailstoggle();
+	$('.detail-toggle, .cb_show_spam').rah_detailstoggle();
 	$('.txp-navigation .prev-next').rah_pagination();
 });
